67b3d3bb3c9b20acdc417ea0abd4b92cea274c99
Skymtes <sild20@student.bth.se>
2023-05-23 11:20:19 +0200

    Finished before cleanup

--- test/test_model.py
+++ test/test_model.py
@@ -1,6 +1,7 @@
 import unittest
 from unittest.mock import patch
 from simulation.model import CommunicationNetwork, TimeVaryingHypergraph
+import os
 
 class TimeVaryingHypergraphTest(unittest.TestCase):
     cn = TimeVaryingHypergraph(
@@ -52,9 +53,18 @@ class TimeVaryingHypergraphTest(unittest.TestCase):
 
 class CommunicationNetworkTest(unittest.TestCase):
     def setUp(self):
+        try:
+            import orjson
+            self.json = "orjson.loads"
+        except:
+            self.json = "json.loads"
+
         self.dummy_file = "test/testfile.json"
         with open(self.dummy_file, "w") as f:
             f.write("")
+        
+    def tearDown(self):
+        os.remove(self.dummy_file)
 
     # Testing if the from_json function works as expected when the list for participants is empty
     def test_from_json_empty_participants(self):
@@ -67,12 +77,13 @@ class CommunicationNetworkTest(unittest.TestCase):
                 }
                 
         # Patch the required functions and objects with the mocks
-        with patch("json.loads", return_value=data) as _:
+        with patch(self.json, return_value=data) as _:
 
             # Create an object with a call to the method to be tested to make sure we get the error we want
             with self.assertRaises(SystemExit) as context:
                 CommunicationNetwork.from_json(self.dummy_file)
-                self.assertEqual("Line: 3, Chan_id: 4. Participants column empty.", str(context.exception))
+            
+            self.assertEqual("Line: 3, Chan_id: 4. Participants column empty.", str(context.exception))
 
     # Testing if the from_json function works as expected when the value for datetime is incorrect
     def test_from_json_wrong_datetime(self):
@@ -85,18 +96,31 @@ class CommunicationNetworkTest(unittest.TestCase):
                 }
 
         # Patch the required functions and objects with the mocks
-        with patch("json.loads", return_value=data) as _:
+        with patch(self.json, return_value=data) as _:
 
             # Create an object with a call to the method to be tested to make sure we get the error we want
             with self.assertRaises(SystemExit) as context:
                 CommunicationNetwork.from_json(self.dummy_file)
-                self.assertEqual("Line: 2, Chan_id: 3. End column not compatible datetime format.", str(context.exception))
+
+            self.assertEqual("Line: 2, Chan_id: 3. End column not compatible datetime format.", str(context.exception))
 
 class CommunicationNetworkTestIntegration(unittest.TestCase):
-    # Testing if the from_json function works as expected when the values are compressed and correct
-    def test_from_json_compressed_integration(self):
-        # Mock the file reading and decompression
-        data = {
+    def setUp(self):
+        try:
+            import orjson
+            self.json = "orjson.loads"
+        except:
+            self.json = "json.loads"
+
+        self.dummy_file_uncompressed = "test/testfile.json"
+        with open(self.dummy_file_uncompressed, "w") as f:
+            f.write("")
+
+        self.dummy_file_compressed = "test/testfile.bz2"
+        with open(self.dummy_file_compressed, "w") as f:
+            f.write("")
+            
+        self.data = {
                 "00": {"end": "2020-02-05T12:49:39", "participants": [0, 1]},
                 "01": {"end": "2020-02-05T12:49:49", "participants": [2, 3]},
                 "02": {"end": "2020-02-05T12:49:59", "participants": [4, 5]},
@@ -111,12 +135,18 @@ class CommunicationNetworkTestIntegration(unittest.TestCase):
                 "11": {"end": "2020-02-05T12:49:34", "participants": [5, 6]},
                 "12": {"end": "2020-02-05T12:49:35", "participants": [9, 8]}
             }
+        
+    def tearDown(self):
+        os.remove(self.dummy_file_compressed)
+        os.remove(self.dummy_file_uncompressed)
 
+    # Testing if the from_json function works as expected when the values are compressed and correct
+    def test_from_json_compressed_integration(self):
         # Patch the required functions and objects with the mocks
-        with patch("json.loads", return_value=data)as _:
+        with patch(self.json, return_value=self.data)as _:
 
             # Create an object with a call to the method to be tested to make sure the mock is read
-            result = CommunicationNetwork.from_json("test/testfile.json")
+            result = CommunicationNetwork.from_json(self.dummy_file_compressed)
 
             # Assert that the object was created with the default name
             self.assertEqual(result.name, None)
@@ -135,28 +165,11 @@ class CommunicationNetworkTestIntegration(unittest.TestCase):
 
     # Testing if the from_json function works as expected when the values are uncompressed and correct
     def test_from_json_uncompressed_integration(self):
-        # Mock the file reading
-        data = {
-                "00": {"end": "2020-02-05T12:49:39", "participants": [0, 1]},
-                "01": {"end": "2020-02-05T12:49:49", "participants": [2, 3]},
-                "02": {"end": "2020-02-05T12:49:59", "participants": [4, 5]},
-                "03": {"end": "2020-02-05T12:49:19", "participants": [6]},
-                "04": {"end": "2020-02-05T12:49:29", "participants": [7, 8, 9]},
-                "05": {"end": "2020-02-05T12:59:39", "participants": [1, 4]},
-                "06": {"end": "2020-02-05T12:59:30", "participants": [2, 7]},
-                "07": {"end": "2020-02-05T12:59:39", "participants": [5, 2]},
-                "08": {"end": "2020-02-05T12:49:31", "participants": [3]},
-                "09": {"end": "2020-02-05T12:49:32", "participants": [8, 11]},
-                "10": {"end": "2020-02-05T12:49:33", "participants": [0, 9]},
-                "11": {"end": "2020-02-05T12:49:34", "participants": [5, 6]},
-                "12": {"end": "2020-02-05T12:49:35", "participants": [9, 8]}
-            }
-
         # Patch the required functions and objects with the mocks
-        with patch("json.loads", return_value=data) as _:
+        with patch(self.json, return_value=self.data) as _:
 
             # Create an object with a call to the method to be tested to make sure the mock is read
-            result = CommunicationNetwork.from_json("test/testfile.json")
+            result = CommunicationNetwork.from_json(self.dummy_file_uncompressed)
 
             # Assert that the object was created with the default name
             self.assertEqual(result.name, None)
