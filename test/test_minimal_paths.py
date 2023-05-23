2c2c3c7a209630658db48965190a44686a20109a
Skymtes <sild20@student.bth.se>
2023-05-23 12:01:03 +0200

    Finished

--- test/test_minimal_paths.py
+++ test/test_minimal_paths.py
@@ -2,9 +2,17 @@ import unittest
 from random import randint
 
 from simulation.model import CommunicationNetwork
-from simulation.minimal_paths import single_source_dijkstra_vertices, single_source_dijkstra_hyperedges, DistanceType
+from simulation.minimal_paths import (
+    single_source_dijkstra_vertices,
+    single_source_dijkstra_hyperedges,
+    DistanceType,
+)
 
-def communication_network_fuzzer():
+
+def generate_random_network():
+    """
+    The function genreates a random network according to the specifications and returns 2 dicts.
+    """
     length = randint(10, 200)
     input_network: dict = {}
     input_timings: dict = {}
@@ -26,32 +34,123 @@ def communication_network_fuzzer():
     return input_network, input_timings
 
 
-class MinimalPath(unittest.TestCase):
-    fuzzed_input = communication_network_fuzzer()
-    cn = CommunicationNetwork(fuzzed_input[0], fuzzed_input[1])
-    cn_dummy = CommunicationNetwork({'h0': ['v0', 'v1'], 'h1': ['v1', 'v9'], 'h2': ['v2', 'v6'], 'h3': ['v3', 'v8'], 'h4': ['v4', 'v3'], 'h5': ['v5', 'v6'], 'h6': ['v6', 'v7'], 'h7': ['v7', 'v0'], 'h9': ['v9', 'v8'], 'h10': ['v10', 'v8']}, {'h0': 176, 'h1': 68, 'h2': 187, 'h3': 163, 'h4': 57, 'h5': 160, 'h6': 111, 'h7': 174, 'h8': 82, 'h9': 49, 'h10': 7})
+class TestMinimalPath(unittest.TestCase):
+    """
+    A test case for the minimal path file.
+    """
 
+    def setUp(self):
+        """
+        Creating a communication network with the random funcion.
+        Creating a communication network with manually selected values.
+        """
+        self.fuzzed_input = generate_random_network()
+        self.com_net = CommunicationNetwork(self.fuzzed_input[0], self.fuzzed_input[1])
+        self.com_net_dummy = CommunicationNetwork(
+            {
+                "h0": ["v0", "v1"],
+                "h1": ["v1", "v9"],
+                "h2": ["v2", "v6"],
+                "h3": ["v3", "v8"],
+                "h4": ["v4", "v3"],
+                "h5": ["v5", "v6"],
+                "h6": ["v6", "v7"],
+                "h7": ["v7", "v0"],
+                "h9": ["v9", "v8"],
+                "h10": ["v10", "v8"],
+            },
+            {
+                "h0": 176,
+                "h1": 68,
+                "h2": 187,
+                "h3": 163,
+                "h4": 57,
+                "h5": 160,
+                "h6": 111,
+                "h7": 174,
+                "h8": 82,
+                "h9": 49,
+                "h10": 7,
+            },
+        )
 
     def test_shortest_same_output(self):
-        result_1 = single_source_dijkstra_vertices(MinimalPath.cn, 'v1', DistanceType.SHORTEST, min_timing=0)
-        result_2 = single_source_dijkstra_hyperedges(MinimalPath.cn, 'v1', DistanceType.SHORTEST, min_timing=0)
-        self.assertEqual(result_1, result_2, 'Single-source Dijkstra implementations are not equivalent')
+        """
+        Testing if the shortest version of the djikstra algorithms are the same.
+        """
+        result_vertices = single_source_dijkstra_vertices(
+            self.com_net, "v1", DistanceType.SHORTEST, min_timing=0
+        )
+        result_hyperedges = single_source_dijkstra_hyperedges(
+            self.com_net, "v1", DistanceType.SHORTEST, min_timing=0
+        )
+        self.assertEqual(
+            result_vertices,
+            result_hyperedges,
+            "Single-source Dijkstra shortest implementations are not equivalent",
+        )
 
     def test_fastest_same_output(self):
-        result_1 = single_source_dijkstra_vertices(MinimalPath.cn, 'v1', DistanceType.FASTEST, min_timing=0)
-        result_2 = single_source_dijkstra_hyperedges(MinimalPath.cn, 'v1', DistanceType.FASTEST, min_timing=0)
-        self.assertEqual(result_1, result_2, 'Single-source Dijkstra implementations are not equivalent')
+        """
+        Testing if the fastest version of the djikstra algorithms are the same.
+        """
+        result_vertices = single_source_dijkstra_vertices(
+            self.com_net, "v1", DistanceType.FASTEST, min_timing=0
+        )
+        result_hyperedges = single_source_dijkstra_hyperedges(
+            self.com_net, "v1", DistanceType.FASTEST, min_timing=0
+        )
+        self.assertEqual(
+            result_vertices,
+            result_hyperedges,
+            "Single-source Dijkstra fastest implementations are not equivalent",
+        )
 
     def test_foremost_same_output(self):
-        result_1 = single_source_dijkstra_vertices(MinimalPath.cn, 'v1', DistanceType.FOREMOST, min_timing=0)
-        result_2 = single_source_dijkstra_hyperedges(MinimalPath.cn, 'v1', DistanceType.FOREMOST, min_timing=0)
-        self.assertEqual(result_1, result_2, 'Single-source Dijkstra implementations are not equivalent')
+        """
+        Testing if the foremost version of the djikstra algorithms are the same.
+        """
+        result_vertices = single_source_dijkstra_vertices(
+            self.com_net, "v1", DistanceType.FOREMOST, min_timing=0
+        )
+        result_hyperedges = single_source_dijkstra_hyperedges(
+            self.com_net, "v1", DistanceType.FOREMOST, min_timing=0
+        )
+        self.assertEqual(
+            result_vertices,
+            result_hyperedges,
+            "Single-source Dijkstra foremost implementations are not equivalent",
+        )
 
     def test_correct_shortest_output(self):
-        self.assertEqual(single_source_dijkstra_vertices(MinimalPath.cn_dummy, 'v4', DistanceType.SHORTEST, min_timing=0), {'v3': 1, 'v8': 2})
+        """
+        Testing if the shortest version of the djikstra vertices algorithm is correct.
+        """
+        self.assertEqual(
+            single_source_dijkstra_vertices(
+                self.com_net_dummy, "v4", DistanceType.SHORTEST, min_timing=0
+            ),
+            {"v3": 1, "v8": 2},
+        )
 
     def test_correct_fastest_output(self):
-        self.assertEqual(single_source_dijkstra_vertices(MinimalPath.cn_dummy, 'v4', DistanceType.FASTEST, min_timing=0), {'v3': 0, 'v8': 106})
+        """
+        Testing if the fastest version of the djikstra vertices algorithm is correct.
+        """
+        self.assertEqual(
+            single_source_dijkstra_vertices(
+                self.com_net_dummy, "v4", DistanceType.FASTEST, min_timing=0
+            ),
+            {"v3": 0, "v8": 106},
+        )
 
     def test_correct_foremost_output(self):
-        self.assertEqual(single_source_dijkstra_vertices(MinimalPath.cn_dummy, 'v4', DistanceType.FOREMOST, min_timing=0), {'v3': 57, 'v8': 163})
+        """
+        Testing if the foremost version of the djikstra vertices algorithm is correct.
+        """
+        self.assertEqual(
+            single_source_dijkstra_vertices(
+                self.com_net_dummy, "v4", DistanceType.FOREMOST, min_timing=0
+            ),
+            {"v3": 57, "v8": 163},
+        )
--- test/test_model.py
+++ test/test_model.py
@@ -3,114 +3,166 @@ from unittest.mock import patch
 from simulation.model import CommunicationNetwork, TimeVaryingHypergraph
 import os
 
-class TimeVaryingHypergraphTest(unittest.TestCase):
-    cn = TimeVaryingHypergraph(
+
+class TestTimeVaryingHypergraph(unittest.TestCase):
+    """
+    A test case for the TimeVaryingHypergraph class.
+    """
+
+    graph = TimeVaryingHypergraph(
         {"h1": ["v1", "v2"], "h2": ["v2", "v3"], "h3": ["v3", "v4"]},
         {"h1": 1, "h2": 2, "h3": 3},
     )
 
-    # Testing if the timings function works as expected when given no input
     def test_timings_no_parameter(self):
-        self.assertEqual(len(self.cn.timings()), 3)
+        """
+        Testing if the timings function works as expected when given no input
+        """
+        self.assertEqual(len(self.graph.timings()), 3)
 
-    # Testing if the timings function works as expected when given correct input
     def test_timings_correct_parameter(self):
-        self.assertEqual(self.cn.timings("h1"), 1)
+        """
+        Testing if the timings function works as expected when given correct input
+        """
+        self.assertEqual(self.graph.timings("h1"), 1)
 
-    # Testing if the timings function works as expected when given incorrect input
     def test_timings_incorrect_paramter(self):
+        """
+        Testing if the timings function works as expected when given incorrect input
+        """
         with self.assertRaises(Exception) as context:
-            self.cn.timings("x1")
+            self.graph.timings("x1")
         self.assertTrue("No hyperedge matches the timing x1" in str(context.exception))
 
-    # Testing if the vertices function works as expected when given no input
     def test_vertices_no_parameter(self):
-        self.assertEqual(len(self.cn.vertices()), 4)
+        """
+        Testing if the vertices function works as expected when given no input
+        """
+        self.assertEqual(len(self.graph.vertices()), 4)
 
-    # Testing if the vertices function works as expected when given correct input
     def test_vertices_correct_parameter(self):
-        self.assertEqual(self.cn.vertices("h1"), {"v1", "v2"})
+        """
+        Testing if the vertices function works as expected when given correct input
+        """
+        self.assertEqual(self.graph.vertices("h1"), {"v1", "v2"})
 
-    # Testing if the vertices function works as expected when given incorrect input
     def test_vertices_incorrect_parameter(self):
+        """
+        Testing if the vertices function works as expected when given incorrect input
+
+        """
         with self.assertRaises(Exception) as context:
-            self.cn.vertices("x1")
+            self.graph.vertices("x1")
         self.assertTrue("Unknown hyperedge x1" in str(context.exception))
 
-    # Testing if the hyperedges function works as expected when given no input
     def test_hyperedges_no_parameter(self):
-        self.assertEqual(len(self.cn.hyperedges()), 3)
+        """
+        Testing if the hyperedges function works as expected when given no input
+        """
+        self.assertEqual(len(self.graph.hyperedges()), 3)
 
-    # Testing if the hyperedges function works as expected when given correct input
     def test_hyperedges_correct_parameter(self):
-        self.assertEqual(self.cn.hyperedges("v1"), {"h1"})
+        """
+        Testing if the hyperedges function works as expected when given correct input
 
-    # Testing if the hyperedges function works as expected when given incorrect input
+        """
+        self.assertEqual(self.graph.hyperedges("v1"), {"h1"})
+
     def test_hyperedges_incorrect_parameter(self):
+        """
+        Testing if the hyperedges function works as expected when given incorrect input
+        """
         with self.assertRaises(Exception) as context:
-            self.cn.hyperedges("x1")
+            self.graph.hyperedges("x1")
         self.assertTrue("Unknown vertex x1" in str(context.exception))
 
-class CommunicationNetworkTest(unittest.TestCase):
+
+class TestCommunicationNetwork(unittest.TestCase):
+    """
+    A test case for the CommunicationNetwork class.
+    """
+
     def setUp(self):
+        """
+        Differentiating between normal json and orjson depending on which is installed and creating a dummy file
+        """
         try:
             import orjson
-            self.json = "orjson.loads"
+
+            self.json_function_used = "orjson.loads"
         except:
-            self.json = "json.loads"
+            self.json_function_used = "json.loads"
 
         self.dummy_file = "test/testfile.json"
         with open(self.dummy_file, "w") as f:
             f.write("")
-        
+
     def tearDown(self):
+        """
+        Removing the file created in the setup
+        """
         os.remove(self.dummy_file)
 
-    # Testing if the from_json function works as expected when the list for participants is empty
     def test_from_json_empty_participants(self):
-        # Mock the file reading and decompression
-        data = {"1": {"end": "2020-02-05T12:49:39", "participants": [0.1]},
-                    "2": {"end": "2020-02-05T12:49:39", "participants": [2, 3]},
-                    "3": {"end": "2020-02-05T12:49:39", "participants": ["2"]},
-                    "4": {"end": "2020-02-05T12:49:59", "participants": []},
-                    "5.3": {"end": "2020-02-05T12:49:59", "participants": [3, 7]}
-                }
-                
-        # Patch the required functions and objects with the mocks
-        with patch(self.json, return_value=data) as _:
+        """
+        Testing if the from_json function works as expected when the list for participants is empty
+        """
+        data = {
+            "1": {"end": "2020-02-05T12:49:39", "participants": [0.1]},
+            "2": {"end": "2020-02-05T12:49:39", "participants": [2, 3]},
+            "3": {"end": "2020-02-05T12:49:39", "participants": ["2"]},
+            "4": {"end": "2020-02-05T12:49:59", "participants": []},
+            "5.3": {"end": "2020-02-05T12:49:59", "participants": [3, 7]},
+        }
 
-            # Create an object with a call to the method to be tested to make sure we get the error we want
+        with patch(self.json_function_used, return_value=data) as _:
             with self.assertRaises(SystemExit) as context:
                 CommunicationNetwork.from_json(self.dummy_file)
-            
-            self.assertEqual("Line: 3, Chan_id: 4. Participants column empty.", str(context.exception))
 
-    # Testing if the from_json function works as expected when the value for datetime is incorrect
+            self.assertEqual(
+                "Line: 3, Chan_id: 4. Participants column empty.",
+                str(context.exception),
+            )
+
     def test_from_json_wrong_datetime(self):
-        # Mock the file reading and decompression
-        data = {"1": {"end": "2020-02-05T12:49:39", "participants": [0.1]},
-                    "2": {"end": "2020-02-05T12:49:39", "participants": [2, 3]},
-                    "3": {"end": "hejsan", "participants": ["2"]},
-                    "4": {"end": "2020-02-05T12:49:59", "participants": [3]},
-                    "5.3": {"end": "2020-02-05T12:49:59", "participants": [3, 7]}
-                }
+        """
+        Testing if the from_json function works as expected when the value for datetime is incorrect
+        """
+        data = {
+            "1": {"end": "2020-02-05T12:49:39", "participants": [0.1]},
+            "2": {"end": "2020-02-05T12:49:39", "participants": [2, 3]},
+            "3": {"end": "hejsan", "participants": ["2"]},
+            "4": {"end": "2020-02-05T12:49:59", "participants": [3]},
+            "5.3": {"end": "2020-02-05T12:49:59", "participants": [3, 7]},
+        }
 
-        # Patch the required functions and objects with the mocks
-        with patch(self.json, return_value=data) as _:
-
-            # Create an object with a call to the method to be tested to make sure we get the error we want
+        with patch(self.json_function_used, return_value=data) as _:
             with self.assertRaises(SystemExit) as context:
                 CommunicationNetwork.from_json(self.dummy_file)
 
-            self.assertEqual("Line: 2, Chan_id: 3. End column not compatible datetime format.", str(context.exception))
+            self.assertEqual(
+                "Line: 2, Chan_id: 3. End column not compatible datetime format.",
+                str(context.exception),
+            )
 
-class CommunicationNetworkTestIntegration(unittest.TestCase):
+
+class TestCommunicationNetworkIntegration(unittest.TestCase):
+    """
+    A test case for the CommunicationNetwork class.
+    """
+
     def setUp(self):
+        """
+        Differentiating between normal json and orjson depending on which is installed.
+        Creating a compressed dummy file and an uncompressed dummy file.
+        Defining the data used for the tests
+        """
         try:
             import orjson
-            self.json = "orjson.loads"
+
+            self.json_function_used = "orjson.loads"
         except:
-            self.json = "json.loads"
+            self.json_function_used = "json.loads"
 
         self.dummy_file_uncompressed = "test/testfile.json"
         with open(self.dummy_file_uncompressed, "w") as f:
@@ -119,70 +171,102 @@ class CommunicationNetworkTestIntegration(unittest.TestCase):
         self.dummy_file_compressed = "test/testfile.bz2"
         with open(self.dummy_file_compressed, "w") as f:
             f.write("")
-            
+
         self.data = {
-                "00": {"end": "2020-02-05T12:49:39", "participants": [0, 1]},
-                "01": {"end": "2020-02-05T12:49:49", "participants": [2, 3]},
-                "02": {"end": "2020-02-05T12:49:59", "participants": [4, 5]},
-                "03": {"end": "2020-02-05T12:49:19", "participants": [6]},
-                "04": {"end": "2020-02-05T12:49:29", "participants": [7, 8, 9]},
-                "05": {"end": "2020-02-05T12:59:39", "participants": [1, 4]},
-                "06": {"end": "2020-02-05T12:59:30", "participants": [2, 7]},
-                "07": {"end": "2020-02-05T12:59:39", "participants": [5, 2]},
-                "08": {"end": "2020-02-05T12:49:31", "participants": [3]},
-                "09": {"end": "2020-02-05T12:49:32", "participants": [8, 11]},
-                "10": {"end": "2020-02-05T12:49:33", "participants": [0, 9]},
-                "11": {"end": "2020-02-05T12:49:34", "participants": [5, 6]},
-                "12": {"end": "2020-02-05T12:49:35", "participants": [9, 8]}
-            }
-        
+            "00": {"end": "2020-02-05T12:49:39", "participants": [0, 1]},
+            "01": {"end": "2020-02-05T12:49:49", "participants": [2, 3]},
+            "02": {"end": "2020-02-05T12:49:59", "participants": [4, 5]},
+            "03": {"end": "2020-02-05T12:49:19", "participants": [6]},
+            "04": {"end": "2020-02-05T12:49:29", "participants": [7, 8, 9]},
+            "05": {"end": "2020-02-05T12:59:39", "participants": [1, 4]},
+            "06": {"end": "2020-02-05T12:59:30", "participants": [2, 7]},
+            "07": {"end": "2020-02-05T12:59:39", "participants": [5, 2]},
+            "08": {"end": "2020-02-05T12:49:31", "participants": [3]},
+            "09": {"end": "2020-02-05T12:49:32", "participants": [8, 11]},
+            "10": {"end": "2020-02-05T12:49:33", "participants": [0, 9]},
+            "11": {"end": "2020-02-05T12:49:34", "participants": [5, 6]},
+            "12": {"end": "2020-02-05T12:49:35", "participants": [9, 8]},
+        }
+
     def tearDown(self):
+        """
+        Removing both files created in the setup
+        """
         os.remove(self.dummy_file_compressed)
         os.remove(self.dummy_file_uncompressed)
 
-    # Testing if the from_json function works as expected when the values are compressed and correct
     def test_from_json_compressed_integration(self):
-        # Patch the required functions and objects with the mocks
-        with patch(self.json, return_value=self.data)as _:
+        """
+        Testing if the from_json function works as expected when the values are compressed and correct
+        """
+        with patch(self.json_function_used, return_value=self.data) as _:
+            com_net = CommunicationNetwork.from_json(self.dummy_file_compressed)
 
-            # Create an object with a call to the method to be tested to make sure the mock is read
-            result = CommunicationNetwork.from_json(self.dummy_file_compressed)
+            self.assertEqual(com_net.name, None)
 
-            # Assert that the object was created with the default name
-            self.assertEqual(result.name, None)
+            self.assertSetEqual(
+                com_net.channels(),
+                {
+                    "00",
+                    "01",
+                    "02",
+                    "03",
+                    "04",
+                    "05",
+                    "06",
+                    "07",
+                    "08",
+                    "09",
+                    "10",
+                    "11",
+                    "12",
+                },
+            )
+            self.assertSetEqual(com_net.channels(0), {"00", "10"})
+            self.assertSetEqual(com_net.channels(5), {"02", "07", "11"})
+            self.assertSetEqual(com_net.channels(9), {"04", "10", "12"})
 
-            # Assert that the inherited functions work as expected
-            self.assertSetEqual(result.channels(), {'00', '01', '02', '03', '04', '05', '06', '07', '08', '09','10', '11', '12'})
-            self.assertSetEqual(result.channels(0), {'00', '10'})
-            self.assertSetEqual(result.channels(5), {'02', '07', '11'})
-            self.assertSetEqual(result.channels(9), {'04', '10', '12'})
+            self.assertSetEqual(
+                com_net.participants(), {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 11}
+            )
+            self.assertSetEqual(com_net.participants("00"), {0, 1})
+            self.assertSetEqual(com_net.participants("04"), {7, 8, 9})
+            self.assertSetEqual(com_net.participants("08"), {3})
 
-            # Assert that the inherited functions work as expected
-            self.assertSetEqual(result.participants(), {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 11})
-            self.assertSetEqual(result.participants("00"), {0, 1})
-            self.assertSetEqual(result.participants("04"), {7, 8, 9})
-            self.assertSetEqual(result.participants("08"), {3})
-
-    # Testing if the from_json function works as expected when the values are uncompressed and correct
     def test_from_json_uncompressed_integration(self):
-        # Patch the required functions and objects with the mocks
-        with patch(self.json, return_value=self.data) as _:
+        """
+        Testing if the from_json function works as expected when the values are uncompressed and correct
+        """
+        with patch(self.json_function_used, return_value=self.data) as _:
+            com_net = CommunicationNetwork.from_json(self.dummy_file_uncompressed)
 
-            # Create an object with a call to the method to be tested to make sure the mock is read
-            result = CommunicationNetwork.from_json(self.dummy_file_uncompressed)
+            self.assertEqual(com_net.name, None)
 
-            # Assert that the object was created with the default name
-            self.assertEqual(result.name, None)
+            self.assertSetEqual(
+                com_net.channels(),
+                {
+                    "00",
+                    "01",
+                    "02",
+                    "03",
+                    "04",
+                    "05",
+                    "06",
+                    "07",
+                    "08",
+                    "09",
+                    "10",
+                    "11",
+                    "12",
+                },
+            )
+            self.assertSetEqual(com_net.channels(0), {"00", "10"})
+            self.assertSetEqual(com_net.channels(5), {"02", "07", "11"})
+            self.assertSetEqual(com_net.channels(9), {"04", "10", "12"})
 
-            # Assert that the inherited functions work as expected
-            self.assertSetEqual(result.channels(), {'00', '01', '02', '03', '04', '05', '06', '07', '08', '09','10', '11', '12'})
-            self.assertSetEqual(result.channels(0), {'00', '10'})
-            self.assertSetEqual(result.channels(5), {'02', '07', '11'})
-            self.assertSetEqual(result.channels(9), {'04', '10', '12'})
-
-            # Assert that the inherited functions work as expected
-            self.assertSetEqual(result.participants(), {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 11})
-            self.assertSetEqual(result.participants("00"), {0, 1})
-            self.assertSetEqual(result.participants("04"), {7, 8, 9})
-            self.assertSetEqual(result.participants("08"), {3})
-
+            self.assertSetEqual(
+                com_net.participants(), {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 11}
+            )
+            self.assertSetEqual(com_net.participants("00"), {0, 1})
+            self.assertSetEqual(com_net.participants("04"), {7, 8, 9})
+            self.assertSetEqual(com_net.participants("08"), {3})
